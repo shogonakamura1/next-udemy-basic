d69b0e86341b5bfb21e8b818b7becc2c5c5247fb
shogonakamura1 <9281461shougo@gmail.com>
2025-12-26 17:00:45 +0900

    ミドルウェアの学習

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export function middleware(request: NextRequest){
+
+  if (!request.nextUrl.pathname.includes('.')) {
+    console.log('ミドルウェアのテスト')
+  }
+  return NextResponse.next()
+}
+  
+export const config = {
+  matcher: ['/blog/:path*']
+}
