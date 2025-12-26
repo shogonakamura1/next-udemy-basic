f2e1cbf3ff466b0ca070505395653e79cc745595
shogonakamura1 <9281461shougo@gmail.com>
2025-12-26 16:08:18 +0900

    paramsの型を指定

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,5 +1,10 @@
-export default async function page({params}) {
-  console.log(params)
+type Params = {
+  params: Promise<{
+    id: string
+  }>
+}
+
+export default async function page({params} : Params) {
   const { id } = await params
 
   return (
