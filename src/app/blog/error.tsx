e094d85d5efbebcd9a77f6b92afeebaec7565625
shogonakamura1 <9281461shougo@gmail.com>
2025-12-26 16:42:45 +0900

    appフォルダ内の特別なファイル

---
+++ src/app/blog/error.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function error() {
+  return (
+    <div>
+      エラーが発生しました
+    </div>
+  )
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,26 @@
+const articles = [
+  { id: "1", title: "タイトル1" },
+  { id: "2", title: "タイトル2" },
+  { id: "3", title: "タイトル3" },
+]
+
+async function fetchArticles(){
+  await new Promise((resolve)=> setTimeout(resolve, 3000))
+//   throw new Error('エラーが発生')
+  return articles
+}
+
+export default async function BlogPage() {
+  const articles = await fetchArticles()
+  return (
+    <div>
+      <ul>
+        { articles.map((article)=>(
+          <li key={article.id}>
+            title: {article.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
